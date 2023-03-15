21c022e68ac66cc3e40bece724c0f6c20838814f
HeicenBergcoding <abhilash@microsec.ai>
2023-03-15 17:52:28 +0530

    simplified spider

--- steamer/steamer/spiders/strider.py
+++ steamer/steamer/spiders/strider.py
@@ -3,14 +3,10 @@ from pathlib import Path
 
 class SteamSpider(scrapy.Spider):
     name = "strider"
-
-    def start_requests(self):
-        urls = [
+    start_urls = [
             'https://store.steampowered.com/search/?sort_by=Reviews_DESC&supportedlang=english&filter=topsellers&ndl=1'
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
-
+    ]
+    
     def parse(self, response):
         filename = 'steam.html'
         Path(filename).write_bytes(response.body)
