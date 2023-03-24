edd2871e642265c4bdf426db7e66ebff1d8ae27b
heisenberg131 <abhilash@microsec.ai>
2023-03-24 10:48:39 +0530

    add rules to follow links

--- steamer/steamer/spiders/strider.py
+++ steamer/steamer/spiders/strider.py
@@ -1,12 +1,30 @@
 import scrapy
-from pathlib import Path
+from scrapy.spiders import CrawlSpider, Rule
+from steamer.items import getSteamerLinkItems,SteamerItem
+from scrapy.linkextractors import LinkExtractor
+
 
 class SteamSpider(scrapy.Spider):
     name = "strider"
-    
-    def start_requests(self):
-        url = "https://quotes.toscrape.com/js/"
-        yield scrapy.Request(url, meta={'playwright': True})
-    
+    rules = (Rule(
+ LinkExtractor(
+ allow='/app/(.+)/',
+ restrict_css='#search_result_container'
+ ),
+ callback='parse_product'
+ ),Rule(LinkExtractor(
+ allow='page=(d+)',
+ restrict_css='.search_pagination_right')
+ ))
+
+    urls = ["https://store.steampowered.com/search/?sort_by=Reviews_DESC&supportedlang=english&filter=globaltopsellers&ndl=1"]
+
     def parse(self, response):
-        pass
+        gameslinks = response.css('//div[@id="search_resultsRows"]')
+        for game in gameslinks:
+            game_link = getSteamerLinkItems()
+            game_link['link'] = game.css('//').get()
+            
+
+
+
